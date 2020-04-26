765adc2f96c63517988c5303e9a8e80d1e22d267
Christian Oette <christian.oette@sportalliance.com>
2020-04-26 14:23:32 +0200

    Initial project

---
+++ src/main/java/com/christianoette/springbatchpracticethree/SpringBatchPracticeThreeApplication.java
@@ -0,0 +1,13 @@
+package com.christianoette.springbatchpracticethree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchPracticeThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchPracticeThreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/christianoette/springbatchpracticethree/SpringBatchPracticeThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christianoette.springbatchpracticethree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPracticeThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
